c2af62e71c90dcaab090e769d53444bf7cd8f24e
Maden51 <madenon123@gmail.com>
2022-07-12 15:26:21 +0300

    fixing gh

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import DriftPage from './components/DriftPage';
 import ForzaPage from './components/ForzaPage';
 import HomePage from './components/HomePage';
 import TimeAttackPage from './components/TimeAttackPage';
 
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
       <Routes>
-        <React.Fragment>
           <Route path="/" end element={<HomePage />} />
           <Route path="/drift" element={<DriftPage />} />
           <Route path="/timeattack" element={<TimeAttackPage />} />
           <Route path="/forza" element={<ForzaPage />} />
-        </React.Fragment>
       </Routes>
-    </BrowserRouter>
   );
 }
+
+export default App;
